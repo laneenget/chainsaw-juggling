6cd75e3cbe4f19ddfb3f2d71ebebedfdf0360aa8
Lane Enget <lane.enget@go.minneapolis.edu>
2020-02-11 14:07:22 -0600

    Define quit function

--- chainsaw_juggling.py
+++ chainsaw_juggling.py
@@ -3,19 +3,22 @@ import sqlite3
 def main():
     conn = sqlite3.connect('chainsaw_db.sqlite') #Create/open db file
     display_menu()
-    command = input('Command: ')
-    command = command.lower
-    if command == 'create':
-        create_row()
-    elif command == 'read':
-        read_row()
-    elif command == 'update':
-        update_row()
-    elif command == 'destroy':
-        delete_row()
-    elif command = 'exit':
-        quit()
-        break
+    while True:
+        command = input('Command: ')
+        command = command.lower
+        if command == 'create':
+            create_row()
+        elif command == 'read':
+            read_row()
+        elif command == 'update':
+            update_row()
+        elif command == 'destroy':
+            delete_row()
+        elif command == 'exit':
+            quit()
+            break
+        else:
+            print('That is not a valid command. Please try again.')
 
 def display_menu():
     print('CRUD')
@@ -24,19 +27,23 @@ def display_menu():
     print('update -- Update a record holder')
     print('destroy -- Delete a record holder')
     print('exit -- Exit program')
+    print('')
 
 def table_init():
     conn.execute('create table records (name text, country text, catches integer)') #Add table
 
-def create_row():
+#def create_row():
 
 def read_row():
     name = input('Enter name of the record holder: ')
     row = conn.execute('select from records values (?)', (name))
     print(row)
 
-def update_row():
+#def update_row():
 
-def delete_row():
+#def delete_row():
 
 def quit():
+    print('Bye!')
+
+main()
