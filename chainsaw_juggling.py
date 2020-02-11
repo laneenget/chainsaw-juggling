418fa713f966448fc2524db36a5d9fe7f5856749
Lane Enget <lane.enget@go.minneapolis.edu>
2020-02-11 16:17:15 -0600

    Define create function

--- chainsaw_juggling.py
+++ chainsaw_juggling.py
@@ -2,20 +2,21 @@ import sqlite3
 
 def main():
     conn = sqlite3.connect('chainsaw_db.sqlite') #Create/open db file
-    display_menu()
+    conn.execute('create table if not exists records (name text, country text, catches integer)') #Add table
+    display_menu() #Call display function
     while True:
-        command = input('Command: ')
-        command = command.lower()
+        command = input('Command: ') #Take user input
+        command = command.lower() #Validate input
         if command == 'create':
-            create_row()
+            create_row(conn) #Call create
         elif command == 'read':
-            read_row(conn)
+            read_row(conn) #Call read
         elif command == 'update':
-            update_row()
+            update_row(conn) #Call update
         elif command == 'destroy':
-            delete_row()
+            delete_row(conn) #Call delete
         elif command == 'exit':
-            quit()
+            print('Bye!') #Quit program
             break
         else:
             print('That is not a valid command. Please try again.')
@@ -29,21 +30,18 @@ def display_menu():
     print('exit -- Exit program')
     print('')
 
-def table_init():
-    conn.execute('create table records (name text, country text, catches integer)') #Add table
-
-#def create_row():
+def create_row(conn):
+    name, country, catches = input('Enter name, country, and catches of the new record holder separated by commas. ')
+    conn.execute('insert into records values (?, ?, ?)', (name, country, catches))
+    conn.commit
 
 def read_row(conn):
-    name = input('Enter name of the record holder: ')
-    row = conn.execute('select from records values (?)', (name))
-    print(row)
+    name = input('Enter name of the record holder: ') #Take user input
+    row = conn.execute('select from records values (?)', (name)) #Query db using input
+    print(row) #Print result
 
 #def update_row():
 
 #def delete_row():
-
-def quit():
-    print('Bye!')
 
 main()
