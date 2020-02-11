2dbe25846c112bcf4732a40d98268673f1956218
Lane Enget <lane.enget@go.minneapolis.edu>
2020-02-11 13:43:25 -0600

    Define main function

--- chainsaw_juggling.py
+++ chainsaw_juggling.py
@@ -2,18 +2,38 @@ import sqlite3
 
 def main():
     conn = sqlite3.connect('chainsaw_db.sqlite') #Create/open db file
+    display_menu()
+    command = input('Command: ')
+    command = command.lower
+    if command == 'create':
+        create_row()
+    elif command == 'read':
+        read_row()
+    elif command == 'update':
+        update_row()
+    elif command == 'destroy':
+        delete_row()
+    elif command = 'exit':
+        quit()
+        break
 
 def display_menu():
     print('CRUD')
-    print('create -- ')
-    print('read -- ')
-    print('update -- ')
-    print('destroy -- ')
+    print('create -- Create a record holder')
+    print('read -- Read a record holder')
+    print('update -- Update a record holder')
+    print('destroy -- Delete a record holder')
+    print('exit -- Exit program')
 
 def table_init():
     conn.execute('create table records (name text, country text, catches integer)') #Add table
 
-def add_row():
+def create_row():
+
+def read_row():
+    name = input('Enter name of the record holder: ')
+    row = conn.execute('select from records values (?)', (name))
+    print(row)
 
 def update_row():
 
